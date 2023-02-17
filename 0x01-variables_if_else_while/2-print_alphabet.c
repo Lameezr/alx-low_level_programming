7561fca4d798388626658f2ed80c806422efab34
lameezr <lameezr17@gmail.com>
2023-02-17 08:05:30 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  * main - A program that prints the alphabet in lowercase
+ *
  * Return: 0 (Success)
  */
 
@@ -11,9 +12,9 @@ int main(void)
 	c = 'a';
 	while
 		(c <= 'z') {
-putchar(c)
-c++;
+			putchar(c);
+			c++;
 		}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
