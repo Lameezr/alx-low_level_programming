ceb3d9e60cfa6b0a0a7ffa8a0fb54b833f93fe16
lameezr <lameezr17@gmail.com>
2023-03-16 05:06:15 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,10 +37,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 
 
-	while (n < len2 && i < (len1 + n));
+	while (n < len2 && i < (len1 + n))
 		s[i++] = s2[j++];
 
-	while (n >= len2 && i < (len1 + len2);
+	while (n >= len2 && i < (len1 + len2))
 		s[i++] = s2[j++];
 
 
